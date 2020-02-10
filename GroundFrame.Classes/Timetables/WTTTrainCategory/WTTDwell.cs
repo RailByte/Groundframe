e2a4b45fce13da31515788f82229b467c29919d8
Tim Caceres <tcaceres@mayowynnebaxter.co.uk>
2020-02-10 14:12:09 +0000

    Tidied up WTTDwell XML parse code

--- GroundFrame.Classes/Timetables/WTTTrainCategory/WTTDwell.cs
+++ GroundFrame.Classes/Timetables/WTTTrainCategory/WTTDwell.cs
@@ -117,62 +117,14 @@ namespace GroundFrame.Classes.Timetables
                     return;
                 }
 
-                int RedSignalMoveOff = XMLMethods.GetValueFromXElement<int>(DwellXML, @"RedSignalMoveOff", 0);
-
-                if (RedSignalMoveOff != 0)
-                {
-                    this.RedSignalMoveOff = new WTTDuration(RedSignalMoveOff);
-                }
-
-                int StationForward = XMLMethods.GetValueFromXElement<int>(DwellXML, @"StationForward", 0);
-
-                if (StationForward != 0)
-                {
-                    this.StationForward = new WTTDuration(StationForward);
-                }
-
-                int StationReverse = XMLMethods.GetValueFromXElement<int>(DwellXML, @"StationReverse", 0);
-
-                if (StationReverse != 0)
-                {
-                    this.StationReverse = new WTTDuration(StationReverse);
-                }
-
-                int TerminateForward = XMLMethods.GetValueFromXElement<int>(DwellXML, @"TerminateForward", 0);
-
-                if (TerminateForward != 0)
-                {
-                    this.TerminateForward = new WTTDuration(TerminateForward);
-                }
-
-                int TerminateReverse = XMLMethods.GetValueFromXElement<int>(DwellXML, @"TerminateReverse", 0);
-
-                if (TerminateReverse != 0)
-                {
-                    this.TerminateReverse = new WTTDuration(TerminateReverse);
-                }
-
-                int Join = XMLMethods.GetValueFromXElement<int>(DwellXML, @"Join", 0);
-
-                if (Join != 0)
-                {
-                    this.Join = new WTTDuration(Join);
-                }
-
-                int Divide = XMLMethods.GetValueFromXElement<int>(DwellXML, @"Divide", 0);
-
-                if (Divide != 0)
-                {
-                    this.Divide = new WTTDuration(Divide);
-                }
-
-                int CrewChange = XMLMethods.GetValueFromXElement<int>(DwellXML, @"CrewChange", 0);
-
-                if (CrewChange != 0)
-                {
-                    this.CrewChange = new WTTDuration(CrewChange);
-                }
-
+                this.RedSignalMoveOff = XMLMethods.GetValueFromXElement<WTTDuration>(DwellXML, @"RedSignalMoveOff", null);
+                this.StationForward = XMLMethods.GetValueFromXElement<WTTDuration>(DwellXML, @"StationForward", null);
+                this.StationReverse = XMLMethods.GetValueFromXElement<WTTDuration>(DwellXML, @"StationReverse", null);
+                this.TerminateForward = XMLMethods.GetValueFromXElement<WTTDuration>(DwellXML, @"TerminateForward", null);
+                this.TerminateReverse = XMLMethods.GetValueFromXElement<WTTDuration>(DwellXML, @"TerminateReverse", null);
+                this.Join = XMLMethods.GetValueFromXElement<WTTDuration>(DwellXML, @"Join", null);
+                this.Divide = XMLMethods.GetValueFromXElement<WTTDuration>(DwellXML, @"Divide", null);
+                this.CrewChange = XMLMethods.GetValueFromXElement<WTTDuration>(DwellXML, @"CrewChange", null);
             }
             catch (Exception Ex)
             {
